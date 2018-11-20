04d5fb6a39a84be38eb9a4c7bd8df8f5b527d45e
richard.liao <richard.liao@program.com.tw>
2018-11-20 11:32:19 +0800

    Updates

---
+++ precache-manifest.508233a0664167105c95c43446ef6e05.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-book/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "80a249600d72daf32a35",
+    "url": "/react-book/static/js/runtime~main.80a24960.js"
+  },
+  {
+    "revision": "4d76abd56bcb4165b53e",
+    "url": "/react-book/static/js/main.4d76abd5.chunk.js"
+  },
+  {
+    "revision": "9681de28577aa8ddecb7",
+    "url": "/react-book/static/js/1.9681de28.chunk.js"
+  },
+  {
+    "revision": "4d76abd56bcb4165b53e",
+    "url": "/react-book/static/css/main.0c12fc1f.chunk.css"
+  },
+  {
+    "revision": "cc52091ed9bb22f0b1721da0f34ac163",
+    "url": "/react-book/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.82e62013919d38ee0e3a53ec0d30a025.js"
+  "/react-book/precache-manifest.508233a0664167105c95c43446ef6e05.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-book/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
